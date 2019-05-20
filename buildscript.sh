a301efa48507acd73c192a76d41c6bfd53090e79
Keerthi Ramanarayan <keerthi.ramanarayan@philips.com>
2019-05-20 17:06:12 -0400

    Publish Code-Coverage output

--- buildscript.sh
+++ buildscript.sh
@@ -3,7 +3,9 @@
 cd /src
 go mod download
 go get -u github.com/jstemmer/go-junit-report
+go get -u github.com/t-yuki/gocover-cobertura
 rm -rf build/ && mkdir -p build
 go build -v -o build/logproxy .
-go test  -coverprofile build/coverage.out -v ./... 2>&1  | tee build/test-result.txt 
+go test  -coverprofile build/coverage.out -covermode count -v ./... 2>&1  | tee build/test-result.txt 
 go-junit-report < build/test-result.txt | tee build/TEST-report.xml
+gocover-cobertura < build/coverage.out > build/coverage-cobertura.xml
