3e6a002e0d898d51e50f80c94226ff184cb69c46
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-05-25 21:18:00 +0200

    Update documentation

--- queue/queue.go
+++ queue/queue.go
@@ -4,8 +4,12 @@ import (
 	"github.com/philips-software/go-hsdp-api/logging"
 )
 
-// Queue implements a logproxy queuer
-// Internally the queue should transform the raw payload to a logging.Resource
+// Queue implements a queue mechanism. The queue can be
+// backed by e.g. RabbitMQ or a simple Go channel. Both
+// of these are provided as part of logproxy.
+// Internally the queue is driven by the Deliverer which
+// transforms the raw payload to a logging.Resource
+// and than pushes it to HSDP logging infrastructure
 type Queue interface {
 	// Start initializes the and returns a stop channel
 	Start() (chan bool, error)
